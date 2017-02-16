ef8df319ca48aa49f992d0806550a8fd658b7342
Christopher Ban <chrisban@me.com>
2017-02-16 01:01:18 -0800

    Initial Commit
    Setting up main page views

---
+++ DotA Tracker/MainPageViewController.swift
@@ -0,0 +1,42 @@
+//
+//  MainPageViewController.swift
+//  DotA Tracker
+//
+//  Created by Christopher Ban on 2/15/17.
+//  Copyright © 2017 cban. All rights reserved.
+//
+
+import UIKit
+
+class MainPageViewController: UIPageViewController, UIPageViewControllerDataSource{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        return nil
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        return nil
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
